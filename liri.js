c1b75a5bd65e93a040e8b8b4a065ddef6b49d560
Wills Grave <will@keydigitalconsulting.com>
2019-10-11 23:35:01 -0400

    Added notes to js file and access to command line arguments code.

--- liri.js
+++ liri.js
@@ -1,7 +1,14 @@
+//Must create own .env file
 require("dotenv").config();
 
+//Access to the npm's
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+//Access to command line arguments
+var [node, file, ...args] = process.argv;
+
+
