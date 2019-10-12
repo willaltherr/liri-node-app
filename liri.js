b3c212562b24d6930aa494b9f49b8c480ae5edd6
Wills Grave <will@keydigitalconsulting.com>
2019-10-11 23:47:22 -0400

    Added code in js to say if no song or movie command line pull default song and movie.

--- liri.js
+++ liri.js
@@ -11,4 +11,23 @@ var spotify = new Spotify(keys.spotify);
 //Access to command line arguments
 var [node, file, ...args] = process.argv;
 
+//Pull Mr. Nobody film if no command line after "movie-this"
+if (args[0] === "movie-this") {
+  if (args[1] === undefined) {
+    getMovie("Mr.+Nobody");
+  }
+  else {
+    getMovie(args.slice(1).join("+"));
+  }
+};
 
+//Pull The Sign song if no command line after "spotify-this-song"
+if (args[0] === "spotify-this-song") {
+  if (argv[1] === undefined) {
+    spotifySong("The Sign");
+  }
+  else {
+    var songTitle = args.slice(1).join(" ");
+    spotifySong(songTitle);
+  }
+};
