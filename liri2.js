073a3c6706d2b9d0144e72f1b82b3a5b9b186a17
Wills Grave <will@keydigitalconsulting.com>
2019-10-14 21:56:44 -0400

    Added the switch statment.

--- liri2.js
+++ liri2.js
@@ -1,4 +1,7 @@
+//Must create own .env file
 require("dotenv").config();
+
+//Access to the npm's
 var fs = require("fs");
 var moment = require("moment");
 
@@ -6,4 +9,27 @@ var axios = require("axios");
 var Spotify = require("node-spotify-api");
 
 var spotifyKeyInfo = require("./keys.js");
+
+// Switch Statement
+var userInput = process.argv;
+var inputTopic = process.argv[2];
+
+
+switch (inputTopic){
+    case "concert-this":
+        bandInfo();
+        break;
+    
+    case "spotify-this-song":
+        songInfo();
+        break;
+    
+    case "movie-this":
+        movieInfo();
+        break;
+
+    case "do-what-it-says":
+        doWhatInfo();
+        break;
+}
 
