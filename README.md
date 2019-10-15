eff0892bdcf5d7c5b8eb06d5dcc3b070332a3e16
Wills Grave <will@keydigitalconsulting.com>
2019-10-15 18:38:19 -0400

    Adding Read.ME file.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # liri-node-app
 LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+1. In this assignment, we will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+2. The app is organized into three sections:
+    - First section includes access to the NPM and creation of variables that will allow us to pull information.  It also inlcudes loading modules.
+    - Second section includes the switch statement.  This includes the user input and input topic variables as well as retrieving arguments with process.argv.
+    - Third section are the commands and search variables that execute the commands to pull the data of what the user is searching.
+
+3. Instructions:
+    - In order to find a concert of a particular band. In the terminal type node liri.js concert-this "band name"
+    - In order to find a info on a particular movie. In the terminal type node liri.js movie-this "movie name"
+    - In order to find a info on a particular song. In the terminal type node liri.js spotify-this-song "song name"
+    - In order to find info based off the random.txt file. In the terminal type node liri.js do-what-it-says
+
+4. Images are located in the image file.
+
+
--- liri.js
+++ liri.js
@@ -2,100 +2,125 @@
 require("dotenv").config();
 
 //Access to the npm's
-var axios = require("axios");
-var Spotify = require('node-spotify-api');
 var fs = require("fs");
-var spotifyKeys = require("./keys.js");
-var spotify = new Spotify(spotifyKeys.spotify);
+var moment = require("moment");
 
-//Access to command line arguments
-var [node, file, ...args] = process.argv;
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 
-//Pull Mr. Nobody film if no command line after "movie-this"
-if (args[0] === "movie-this") {
-  if (args[1] === undefined) {
-    getMovie("Mr.+Nobody");
+var spotifyKeyInfo = require("./keys.js");
+
+// Add the Switch Statement
+var userInput = process.argv;
+var inputTopic = process.argv[2];
+
+
+switch (inputTopic){
+    case "concert-this":
+        bandInfo();
+        break;
+    
+    case "spotify-this-song":
+        songInfo();
+        break;
+    
+    case "movie-this":
+        movieInfo();
+        break;
+
+    case "do-what-it-says":
+        doWhatInfo();
+        break;
+}
+
+//Concert Command & Search Variable
+function bandInfo(){
+  var bandName = "";
+  for (var i = 3; i < userInput.length; i++){
+      if (i > 3 && i < userInput.length){
+          bandName = bandName + "+" + userInput[i];
+      }
+      else{
+          bandName += userInput[i];
+      }
   }
-  else {
-    getMovie(args.slice(1).join("+"));
-  }
+  var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=00e38aff0f13b24e9ff246b1e74f33b1";
+    
+  console.log(queryURL); 
+
+  axios.get(queryURL).then(
+      function(bandResponse){
+          console.log("Venue: " + bandResponse.data[0].venue.name);
+          console.log("City: " + bandResponse.data[0].venue.city);
+          console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+      }
+  );
 };
 
-//Pull The Sign song if no command line after "spotify-this-song"
-if (args[0] === "spotify-this-song") {
-  if (args[1] === undefined) {
-    spotifySong("The Sign");
+//Spotify Command & Search Variable
+function songInfo(){
+  var songName = "";
+  for (var i = 3; i < userInput.length; i++){
+      if (i > 3 && i < userInput.length){
+          songName = songName + "+" + userInput[i];
+      }
+      else{
+          songName += userInput[i];
+      }
   }
-  else {
-    var songTitle = args.slice(1).join(" ");
-    spotifySong(songTitle);
-  }
+  var spotify = new Spotify({
+    id: spotifyKeyInfo["spotify"].id,
+    secret: spotifyKeyInfo["spotify"].secret
+});
+
+spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=5', function(error, songResponse) {
+    if (error){
+        return console.log(error);
+    }
+    console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
+    console.log("Song: " + songResponse.tracks.items[0].name);
+    console.log("URL: " + songResponse.tracks.items[0].preview_url);
+    console.log("Album: " + songResponse.tracks.items[0].album.name);
+});
 };
 
-//Command line "do-what-it-says" random.txt will perform one of two functions
-if (args[0] === "do-what-it-says") {
-  fs.readFile("random.txt", "utf8", function (error, data) {
+//Movie Command & Search Variable
+function movieInfo(){
+  var movieName = "";
+  for (var i = 3; i < userInput.length; i++){
+      if (i > 3 && i < userInput.length){
+          movieName = movieName + "+" + userInput[i];
+      }
+      else{
+          movieName += userInput[i];
+      }
+  }
+  var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=dbb2860a";
 
-    //Provide error message for errors
+  axios.get(queryURL).then(
+      function(movieResponse){
+          console.log("Title: " + movieResponse.data.Title);
+          console.log("Year: " + movieResponse.data.Year);
+          console.log("Rated: " + movieResponse.data.imdbRating);
+          console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+          console.log("Country: " + movieResponse.data.Country);
+          console.log("Language: " + movieResponse.data.Language);
+          console.log("Plot: " + movieResponse.data.Plot);
+          console.log("Actors: " + movieResponse.data.Actors);
+      }
+  );
+};
+
+//FS Command & Search Variable
+function doWhatInfo() {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
       return console.log(error);
     }
-
-    dataArr = data.split(",");
-    if (dataArr[0] === "movie-this") {
-      if (dataArr[1] === undefined) {
-        getMovie("Mr.+Nobody")
+      var output = data.split(",");
+      for (var i = 0; i < output.length; i++) {
+          console.log(output[i]);
       }
-      else {
-        getMovie(dataArr[1].split().join("+"))
-      }
-    };
-    
-    if (dataArr[0] === "spotify-this-song") {
-      if (dataArr[1] === undefined) {
-        spotifySong("The Sign")
-      }
-      else {
-        spotifySong(dataArr[1])
-      }
-    };
-  });
-};
-
-//Pulls 5 tracks from Spotify
-function spotifySong(songName) {
-  spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, data) {
-    if(err) {
-      return console.log('Error occurred: ' + err);
-    }
-
-    data.tracks.items.forEach(function (element) {
-      console.log("");
-      console.log(`Artist: ${element.artist[0].name}`);
-      console.log(`Song: ${songName}`);
-      console.log(`Spotify Preview Link: ${element.preview_url}`);
-      console.log(`Album: ${element.album.name}`);
-    });
-  })
-};
-
-//Movie function to pull OMDB's database
-function getMovie(movieName) {
-  axios
-    .get(`http://www.omdbapi.com/?t=${movieName}&apikey=dbb2860a`)
-    .then(function (movie) {
-      console.log("");
-      console.log(`Title: ${movie.data.Title}`);
-      console.log(`Released: ${movie.data.Year}`);
-      console.log(`IMDB Rating: ${movie.data.Ratings[0].Value}`);
-      console.log(`Rotten Tomatoes Rating: ${movie.data.Ratings[1].Value}`);
-      console.log(`Produced in: ${movie.data.Country}`);
-      console.log(`Language in: ${movie.data.Language}`);
-      console.log(`Plot: ${movie.data.Plot}`);
-      console.log(`Starring: ${movie.data.Actors}`);
-    })
-    .catch(function (err) {
-      console.log(err);
     });
 };
-
